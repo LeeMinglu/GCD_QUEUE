53f4d9d80b16b799be218c50d8ec2a2db9ac655f
LeeMinglu <402007353@qq.com>
2016-01-01 21:45:26 +0800

    主线程同步会造成死锁

--- GCD_QUEUE/GCD_QUEUE/ViewController.m
+++ GCD_QUEUE/GCD_QUEUE/ViewController.m
@@ -17,7 +17,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self demo1];
+//    [self demo1];
+//    [self demo2];
+//    [self demo3];
+    
+    [self demo4];
+}
+
+//主队列同步，会造成死锁
+- (void)demo4 {
+    
+    NSLog(@"start");
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    
+    // 主线程&主队列相互等待，造成死锁！
+    dispatch_sync(queue, ^{
+        NSLog(@"%@", [NSThread currentThread]);
+    });
+    NSLog(@"come here");
+}
+
+
+//并发队列异步执行
+
+- (void)demo3 {
+    dispatch_queue_t queue = dispatch_queue_create("socerer.luoriver", DISPATCH_QUEUE_CONCURRENT);
+    
+    for (int i = 0 ; i < 100; i++) {
+        dispatch_async(queue, ^{
+            NSLog(@"%@ ....%d",[NSThread currentThread], i);
+        });
+        
+//        NSLog(@"for  ..%d", i);
+    }
+    
+    NSLog(@"come here");
 }
 
 
